d378626c19de9f24e6fd8e24f0a33e82a9cefbbe
Liam Idrovo <liamidrovo@gmail.com>
2023-07-20 01:09:16 -0700

    film_processing lambda deployment

---
+++ ops/bucketAsSourceFilmProcessing.sh
@@ -0,0 +1,28 @@
+FUNCARN=$(aws cloudformation describe-stacks \
+  --stack-name "language-recognition" \
+  --query 'Stacks[0].Outputs[?OutputKey==`FilmProcessingFunctionArn`].OutputValue' \
+  --output text \
+)
+
+JSON=$(cat <<-EOF
+  {
+    "LambdaFunctionConfigurations": [
+      {
+        "LambdaFunctionArn": "${FUNCARN}",
+        "Events": [
+          "s3:ObjectCreated:*"
+        ]
+      }
+    ]
+  }
+EOF
+)
+
+echo $JSON
+
+aws s3api \
+  put-bucket-notification-configuration \
+  --bucket="language-bucket-liam-idrovo" \
+  --notification-configuration "$JSON" \
+
+  
---
+++ serverless/whisper/whisper.py
@@ -0,0 +1,28 @@
+import boto3
+
+
+def lambda_handler(event, context):
+    print("event:", event)
+
+    objectKey = event["Records"][0]["s3"]["object"]["key"]
+
+    return {
+        "statusCode": 200,
+        "body": {
+            "message": "File uploaded successfully",
+        },
+    }
+
+    # Create an S3 client
+    s3_client = boto3.client("s3")
+
+    # Get the file from the request body
+    file_content = event["body"]
+
+    # Upload the file to S3
+    s3_client.put_object(Body=file_content, Bucket=s3_bucket_name, Key="example.mp4")
+
+    return {"statusCode": 200, "body": "File uploaded successfully"}
+
+
+print("ran")
