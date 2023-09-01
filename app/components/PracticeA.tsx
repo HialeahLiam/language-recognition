1d1b3a054d6e9f3543a82ae64ed7fa8e07637606
Liam Idrovo <liamidrovo@gmail.com>
2023-08-31 17:13:03 -0700

    #11 deleted multiple choice

--- app/components/PracticeA.tsx
+++ app/components/PracticeA.tsx
@@ -19,7 +19,6 @@ interface Props {
 
 function PracticeA({ snippet, onNextSnippet }: Props) {
   const [currentWord, setCurrentWord] = useState("");
-  const [multipleChoice, setMultipleChoice] = useState(false);
   const [guessRevealed, setGuessRevealed] = useState(false);
   const [guess, setGuess] = useState("");
   const [progressInterval, setProgressInterval] = useState<NodeJS.Timer | null>(
@@ -57,7 +56,6 @@ function PracticeA({ snippet, onNextSnippet }: Props) {
     const videoElement = video.current as unknown as HTMLVideoElement;
     setProgressInterval(
       setInterval(() => {
-        console.log("inter");
         if (!videoElement.paused) {
           updateCurrentWord(videoElement.currentTime);
         }
@@ -72,13 +70,7 @@ function PracticeA({ snippet, onNextSnippet }: Props) {
     setCurrentWord("");
   }
 
-  function handleNeedHelp() {
-    setGuess("");
-    setMultipleChoice(true);
-  }
-
   function reset() {
-    setMultipleChoice(false);
     setGuessRevealed(false);
     setGuess("");
     setGuessCorrect(null);
@@ -97,7 +89,6 @@ function PracticeA({ snippet, onNextSnippet }: Props) {
         className="h-60 mb-20"
         src={snippet.videoSrc}
       ></video>
-
       {guessCorrect !== null && guessCorrect && <p>Correct!</p>}
       {guessCorrect !== null && !guessCorrect && <p>Incorrect</p>}
       <p className="mb-2">
@@ -132,73 +123,24 @@ function PracticeA({ snippet, onNextSnippet }: Props) {
           );
         })}
       </p>
-      {multipleChoice ? (
-        <div>
-          <fieldset>
-            <legend className="sr-only">guess</legend>
-            <div className="space-y-5">
-              {snippet.wordChoices.map((choice) => (
-                <div key={choice} className="relative flex items-start">
-                  <div className="flex h-6 items-center">
-                    <input
-                      id={choice}
-                      aria-describedby={`${choice}-description`}
-                      onChange={(e) => setGuess(e.target.value)}
-                      name="guess"
-                      value={choice}
-                      type="radio"
-                      defaultChecked={choice === "small"}
-                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                    />
-                  </div>
-                  <div className="ml-3 text-sm leading-6">
-                    <label
-                      htmlFor={choice}
-                      className="font-medium text-gray-900"
-                    >
-                      {choice}
-                    </label>{" "}
-                  </div>
-                </div>
-              ))}
-            </div>
-          </fieldset>
-          <button
-            type="button"
-            onClick={handleCheckGuess}
-            className="rounded-full bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Check
-          </button>
-        </div>
-      ) : (
-        <div>
-          <input
-            name="guess"
-            id="guess"
-            className="mb-4 mr-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            onChange={(e) => setGuess(e.target.value)}
-            value={guess}
-            autoComplete="off"
-          />
-          <button
-            type="button"
-            onClick={handleCheckGuess}
-            className="rounded-full bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Check
-          </button>
-        </div>
-      )}
-      {!multipleChoice && (
+
+      <div>
+        <input
+          name="guess"
+          id="guess"
+          className="mb-4 mr-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          onChange={(e) => setGuess(e.target.value)}
+          value={guess}
+          autoComplete="off"
+        />
         <button
           type="button"
-          onClick={handleNeedHelp}
-          className="rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          onClick={handleCheckGuess}
+          className="rounded-full bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          I need help
+          Check
         </button>
-      )}
+      </div>
       <button
         type="button"
         onClick={handlePlay}
